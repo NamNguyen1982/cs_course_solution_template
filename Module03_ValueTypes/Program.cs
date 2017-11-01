57c1afa7922a119c784ea785c0ab942276e84aa3
NamNguyen1982 <namn1982@gmail.com>
2017-11-01 15:48:33 +0100

    .

--- Module03_ValueTypes/Program.cs
+++ Module03_ValueTypes/Program.cs
@@ -5,6 +5,23 @@
         static void Main(string[] args)
         {
 
+            // no go
+            //int i = 50000000;
+            //short j = i;
+
+            //int i = 50000000;
+            //short j = (short)i;
+
+
+            int yy = 5000;
+            //checked
+            //{
+            //    short uu = (short)yy;
+
+            //}
+            short uu = System.Convert.ToInt16(yy);
+
+
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 System.Console.Write("Press any key to continue . . . ");
