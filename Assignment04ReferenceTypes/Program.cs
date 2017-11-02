e2c89fcf7c952b05d26326b8771cea5cd640e352
NamNguyen1982 <namn1982@gmail.com>
2017-11-02 11:43:50 +0100

    .

---
+++ Assignment04ReferenceTypes/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment04ReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialiserer Array
+            int[] månedsløn;
+
+            månedsløn = new int[12];
+
+            månedsløn[0] = 100;
+            månedsløn[1] = 200;
+            månedsløn[2] = 300;
+            månedsløn[3] = 400;
+            månedsløn[4] = 500;
+            månedsløn[5] = 600;
+            månedsløn[6] = 700;
+            månedsløn[7] = 800;
+            månedsløn[8] = 900;
+            månedsløn[9] = 1000;
+            månedsløn[10] = 1100;
+            månedsløn[11] = 1200;
+
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+
+                // udskriver i oprindelig orden
+                int beløb = månedsløn[i];
+                System.Console.WriteLine("Denne måned har du fået= " + beløb);
+
+
+            }
+
+            // Reverse metode benyttes
+            Array.Reverse(månedsløn);
+
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+
+                //udskriver i omvendt rækkefølge
+                int beløb = månedsløn[i];
+                System.Console.WriteLine("Denne måned har du fået= " + beløb);
+
+            }
+
+            //sorterer array elementer efter ascending
+            Array.Sort(månedsløn);
+
+
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+
+                //udskriver oprindelig rækkefølge
+                int beløb = månedsløn[i];
+                System.Console.WriteLine("Denne måned har du fået= " + beløb);
+
+
+            }
+
+            Array.Resize(ref månedsløn, 13);
+
+            månedsløn[12] = 12;
+
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+
+                //udskriver oprindelig rækkefølge
+                int beløb = månedsløn[i];
+                System.Console.WriteLine("Denne måned har du fået= " + beløb);
+
+
+            }
+
+
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+        class Dav
+        {
+        public int tal;
+
+}
+    }
+}
--- Module04_ReferenceTypes/Program.cs
+++ Module04_ReferenceTypes/Program.cs
@@ -4,6 +4,48 @@
     {
         static void Main(string[] args)
         {
+            Kat k1;
+            k1.Navn = "a";
+            k1.Alder = 6;
+
+            Kat k2;
+            k2.Navn = "b";
+            k2.Alder = 10;
+
+            k1 = k2;
+
+            k1.Alder = 100;
+
+            Hund h1 = new Hund();
+            h1.Navn = "x";
+            h1.AntalBen = 3;
+
+            Hund h2 = new Hund();
+            h2.Navn = "y";
+            h2.AntalBen = 10;
+
+            h1 = h2;
+            System.Console.WriteLine("{0} {1}", (string)h1.Navn, (int)h1.AntalBen);
+            System.Console.WriteLine("{0} {1}", (string)h2.Navn, (int)h2.AntalBen);
+
+            int[] månedsløn;
+
+            månedsløn = new int[5];
+
+            månedsløn[0] = 100;
+            månedsløn[1] = 200;
+            månedsløn[2] = 300;
+            månedsløn[3] = 400;
+            månedsløn[4] = 500;
+
+            decimal gns2 = 0;
+
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                gns2 += månedsløn[i];
+                gns2 = gns2 / månedsløn.Length;
+                System.Console.WriteLine("resultat = " + gns2.ToString("n2"));
+            }
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
@@ -11,5 +53,17 @@
                 System.Console.ReadKey();
             }
         }
+
+        struct Kat
+        {
+            public string Navn;
+            public int Alder;
+        }
+
+        class Hund
+        {
+            public string Navn;
+            public int AntalBen;
+         }
     }
 }
