c707f7fcddb9ddbe2da1a828aad760259bdec2ea
Nam Nguyen <namn1982@gmail.com>
2017-11-14 09:45:10 +0100

    .

---
+++ PersonKlasse/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonKlasse
+{
+   public class Person
+    {
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int alder;
+
+        public int Alder
+        {
+            get { return alder; }
+            set { alder = value; }
+        }
+
+
+
+
+
+    }
+}
