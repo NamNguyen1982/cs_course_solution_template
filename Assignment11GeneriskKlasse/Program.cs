f9d78095bc2d0cb032251965ccddc35acad556c3
Nam <namn1982@gmail.com>
2017-11-27 15:32:25 +0100

    .

---
+++ Assignment11GeneriskKlasse/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11GeneriskKlasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Person1 p1 = new Person1();
+            p1.Tilføj(new Instruktør());
+
+            
+
+        }
+    }
+
+    public class Person
+    {
+
+        public string ForNavn { get; set; }
+        public string EfterNavn { get; set; }
+
+        public int Alder { get; set; }
+        public int Cpr { get; set; }
+
+                
+
+      }
+
+    public class Instruktør : Person
+{
+
+        public int NøgleId { get; set; }
+        public string KlasseVærelse { get; set; }
+
+    }
+
+    public class Kursist : Person
+    {
+
+        public int KursistId { get; set; }
+        public int Timer { get; set; }
+    }
+    
+    public class Person1
+    {
+
+        private List<Person> personer = new List<Person>();
+
+        public void Tilføj(Person person)
+        {
+            personer.Add(person);
+
+        }
+
+
+    }
+
+
+}
