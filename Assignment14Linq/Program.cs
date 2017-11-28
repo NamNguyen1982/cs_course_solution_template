d25e42d6de8fb1d896838354ee4cb24e48a41665
Nam <namn1982@gmail.com>
2017-11-28 15:41:41 +0100

    .

---
+++ Assignment14Linq/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment14Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new PersonNuGetPackage.PersonRepositoryRandom();
+            var people = r.GetPeople(100);
+
+            foreach (var person in people)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            var sort = from x in people orderby x.Name select people;
+
+            foreach (var item in people)
+            {
+                Console.WriteLine(sort);
+            }
+
+
+            Console.ReadLine();
+
+            //var rr = new PersonNuGetPackage.PersonRepositoryRandom().GetPeople(100);
+            ////var people2 = rr.GetPeople();
+            //foreach (var person in rr)
+            //{
+            //    Console.WriteLine(person.Name);
+            //}
+            //Console.ReadLine();
+
+            //var sort = rr.OrderBy(i => i.Name);
+            //var sort1 = from x in rr orderby x.Name select rr;
+
+            //var sort = from p in rr orderby p.Name select p;
+            //foreach (var item in sort)
+            //{
+            //    Console.WriteLine(sort);
+            //}
+
+            //var sort = rr.OrderBy(p => p.Name);
+            //foreach (var p.name in sort)
+            //{
+            //    Console.WriteLine(sort.);
+            //}
+
+
+            //var sort = people2.OrderBy(i => i.Name);
+            //var sort1 = from 
+            //foreach (var item in sort)
+            //{
+            //    Console.WriteLine(people2.);
+            //}
+
+
+
+
+        }
+    }
+}
---
+++ Assignment14Linq/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Assignment14Linq")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assignment14Linq")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("100e704e-3295-41e6-8eca-6d5f381e13b8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
